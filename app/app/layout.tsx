7d204a58cbbdd88a9be571e7d4c73b661931450f
Apilaash <apilaashyoharan1@gmail.com>
2026-04-10 21:54:31 -0400

    remove comments

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
@@ -9,16 +8,6 @@ import { Provider } from "react-redux";
 import { store } from "@/lib/reduxStore";
 import { MdMenu } from "react-icons/md";
 import { supabase } from "@/lib/supabase/client";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
